d5f70ba68a12268eb2efcfb5f345a08d205ff056
Prince Kumar Sharma <sittu9129@gmail.com>
2026-01-09 12:29:05 +0530

    Update Signup.jsx

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -3,11 +3,11 @@ import logo from "../assets/logo.svg"
 import {useNavigate} from "react-router-dom"
 import { authDataContext } from '../context/AuthContext'
 import axios from "axios"
-import { userDataContext } from '../context/userContext'
+import { UserDataContext } from '../context/UserContext'
 function Signup() {
   let [show,setShow]=useState(false)
   let {serverUrl}=useContext(authDataContext)
-  let {userData,setUserData}=useContext(userDataContext)
+  let {userData,setUserData}=useContext(UserDataContext)
   let navigate=useNavigate()
   let [firstName,setFirstName]=useState("")
   let [lastName,setLastName]=useState("")
