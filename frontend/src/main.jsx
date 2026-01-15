f0d562e3c035368cecbe858b8a4789cc92ee2e36
Prince Kumar Sharma <sittu9129@gmail.com>
2026-01-15 11:43:08 +0530

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,19 +1,18 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import './index.css';
-import App from './App.jsx';
-import { BrowserRouter } from 'react-router-dom';
-import AuthContext from './context/AuthContext.jsx';
-import UserContext from './context/UserContext.jsx';
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import { BrowserRouter } from 'react-router-dom'
+import AuthContext from './context/AuthContext.jsx'
+import UserContext from './context/UserContext.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <BrowserRouter>
-      <AuthContext>
-        <UserContext>
-          <App />
-        </UserContext>
-      </AuthContext>
-    </BrowserRouter>
-  </StrictMode>
-);
+ <BrowserRouter>
+ <AuthContext>
+<UserContext>
+    <App />
+</UserContext>
+</AuthContext>
+</BrowserRouter>
+ 
+)
