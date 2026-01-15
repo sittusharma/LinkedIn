b42b943453b8b5d7e6cc561df534ed85230e5a99
Prince Kumar Sharma <sittu9129@gmail.com>
2026-01-15 11:39:24 +0530

    Update AuthContext.jsx

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -1,18 +1,16 @@
-import React, { createContext, useMemo } from 'react'
-
-export const authDataContext = createContext()
-
-function AuthContext({ children }) {
-  const serverUrl = "https://linkedin-backend-wmse.onrender.com"
-
-  const value = useMemo(() => ({
-    serverUrl
-  }), [])
-
+import React, { createContext } from 'react'
+export const authDataContext=createContext()
+function AuthContext({children}) {
+const serverUrl="http://localhost:8000"
+    let value={
+        serverUrl
+    }
   return (
-    <authDataContext.Provider value={value}>
-      {children}
-    </authDataContext.Provider>
+    <div>
+     <authDataContext.Provider value={value}> 
+     {children}
+     </authDataContext.Provider> 
+    </div>
   )
 }
 
