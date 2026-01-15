e95b539ac53cddacf5e9358a1875ba48fa5dcb57
Prince Kumar Sharma <sittu9129@gmail.com>
2026-01-15 11:31:14 +0530

    Update connection.routes.js

--- backend/routes/connection.routes.js
+++ backend/routes/connection.routes.js
@@ -1,10 +1,15 @@
 import express from "express"
+import { acceptConnection, getConnectionRequests, getConnectionStatus, getUserConnections, rejectConnection, removeConnection, sendConnection } from "../controllers/connection.controllers.js"
 import isAuth from "../middlewares/isAuth.js"
-import { clearAllNotification, deleteNotification, getNotifications } from "../controllers/notification.controllers.js"
+let connectionRouter=express.Router()
 
-let notificationRouter=express.Router()
+connectionRouter.post("/send/:id",isAuth,sendConnection)
+connectionRouter.put("/accept/:connectionId",isAuth,acceptConnection)
+connectionRouter.put("/reject/:connectionId",isAuth,rejectConnection)
+connectionRouter.get("/getstatus/:userId",isAuth,getConnectionStatus)
+connectionRouter.delete("/remove/:userId",isAuth,removeConnection)
+connectionRouter.get("/requests",isAuth,getConnectionRequests)
+connectionRouter.get("/",isAuth,getUserConnections)
 
-notificationRouter.get("/get",isAuth,getNotifications)
-notificationRouter.delete("/deleteone/:id",isAuth,deleteNotification)
-notificationRouter.delete("/",isAuth,clearAllNotification)
-export default notificationRouter
+
+export default connectionRouter
