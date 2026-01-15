c3269c76b64cbb6ff7e2aec7434b10cfa8400da3
Prince Kumar Sharma <sittu9129@gmail.com>
2026-01-15 11:50:54 +0530

    Update Network.jsx

--- frontend/src/pages/Network.jsx
+++ frontend/src/pages/Network.jsx
@@ -8,7 +8,7 @@ import { RxCrossCircled } from "react-icons/rx";
 
 import io from "socket.io-client"
 
-const socket=io("http://localhost:8000")
+const socket=io("https://linkedin-backend-wmse.onrender.com")
 function Network() {
 let {serverUrl}=useContext(authDataContext)
 let [connections,setConnections]=useState([])
